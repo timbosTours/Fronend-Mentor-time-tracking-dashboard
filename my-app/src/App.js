ab894e4ce7a7e61c17c4717514eae8585356d4d7
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-10-23 11:39:08 +1100

    adding h1's

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,25 +13,46 @@ function App() {
 }
 
 const User = () => {
-  <div className="user"></div>
+  <div className="user">
+    <h1>Report for</h1>
+    <p></p>
+  </div>
 }
 const Work = () => {
-  <div className="work"> </div>
+  <div className="work">
+    <h1>Work</h1>
+    <p></p>
+  </div>
 }
 const Play = () => {
-  <div className="play"></div>
+  <div className="play">
+    <h1>Play</h1>
+    <p></p>
+  </div>
 }
 const Study = () => {
-  <div className="study"></div>
+  <div className="study">
+    <h1>Study</h1>
+    <p></p>
+  </div>
 }
 const Exercise = () => {
-  <div className="excersice"></div>
+  <div className="excersice">
+    <h1>Exercise</h1>
+    <p></p>
+  </div>
 }
 const Social = () => {  
-  <div className="social"></div>
+  <div className="social">
+    <h1>Social</h1>
+    <p></p>
+  </div>
 }
 const Selfcare = () => {
-  <div className="selfcare"></div>
+  <div className="selfcare">
+    <h1>Selfcare</h1>
+    <p></p>
+  </div>
 }
 
 export default App;
