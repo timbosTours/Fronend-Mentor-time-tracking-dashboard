75944f61314ba243c09a0e9585ec21781d9cfed7
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-10-24 15:27:23 +1100

    styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,57 +2,65 @@ import './App.css';
 
 function App() {
   return (
-    <User />,
-  <Work />,
-  <Play />,
-  <Study />,
-  <Exercise />,
-  <Social />,
-    <Selfcare/>
+    <main>
+    <User />
+  <Work />
+  <Play />
+  <Study />
+  <Exercise />
+  <Social />
+      <Selfcare />
+    </main>
   );
 }
 
 const User = () => {
-  <div className="user">
-    <h1>Report for</h1>
-    <p></p>
-  </div>
+  return (
+    <div className="user content-box">
+      <h1>Report for</h1>
+      <p></p>
+    </div>);
 }
 const Work = () => {
-  <div className="work">
+  return (
+  <div className="work content-box">
     <h1>Work</h1>
     <p></p>
   </div>
-}
+)}
 const Play = () => {
-  <div className="play">
+  return (
+  <div className="play content-box">
     <h1>Play</h1>
     <p></p>
   </div>
-}
+)}
 const Study = () => {
-  <div className="study">
+  return (
+  <div className="study content-box">
     <h1>Study</h1>
     <p></p>
   </div>
-}
+)}
 const Exercise = () => {
-  <div className="excersice">
+  return (
+  <div className="excersice content-box">
     <h1>Exercise</h1>
     <p></p>
   </div>
-}
+)}
 const Social = () => {  
-  <div className="social">
+  <div className="social content-box">
     <h1>Social</h1>
     <p></p>
   </div>
 }
 const Selfcare = () => {
-  <div className="selfcare">
+  return (
+  <div className="selfcare content-box">
     <h1>Selfcare</h1>
     <p></p>
   </div>
-}
+)}
 
 export default App;
