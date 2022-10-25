61f0fea20d3e6574d73b583837fd6589e5c45dfc
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-10-26 07:31:09 +1100

    added icon images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
-import './data.json';
-import avatar from './images/image-jeremy.png'
+import data from './data.json';
+import avatar from './images/image-jeremy.png';
+import work from './images/icon-work.svg';
+import play from './images/icon-play.svg';
+import study from './images/icon-study.svg';
+import exercise from './images/icon-exercise.svg';
+import social from './images/icon-social.svg';
+import selfcare from './images/icon-self-care.svg';
 
 
 function App() {
@@ -28,47 +34,48 @@ const User = () => {
 const Work = () => {
   return (
   <div className="work content-box">
-    <img src="./" alt="" />
-    <h1>Work</h1>
+    <img src={ work } alt="work icon" />
+    <h2>work</h2>
     <p></p>
   </div>
 )}
 const Play = () => {
   return (
   <div className="play content-box">
-    <img src="./" alt="" />
-    <h1>Play</h1>
+    <img src={ play } alt="play icon" />
+    <h3>Play</h3>
     <p></p>
   </div>
 )}
 const Study = () => {
   return (
   <div className="study content-box">
-    <img src="./" alt="" />
-    <h1>Study</h1>
+    <img src={ study } alt="study icon" />
+    <h4>Study</h4>
     <p></p>
   </div>
 )}
 const Exercise = () => {
   return (
   <div className="excersice content-box">
-    <img src="./" alt="" />
-    <h1>Exercise</h1>
+    <img src={ exercise } alt="exercise icon" />
+    <h5>Exercise</h5>
     <p></p>
   </div>
 )}
-const Social = () => {  
+const Social = () => { 
+  return(
   <div className="social content-box">
-    <img src="./" alt="" />
-    <h1>Social</h1>
+    <img src={ social } alt="social icon" />
+    <h6>Social</h6>
     <p></p>
   </div>
-}
+)}
 const Selfcare = () => {
   return (
   <div className="selfcare content-box">
-    <img src="./" alt="" />
-    <h1>Selfcare</h1>
+    <img src={ selfcare } alt="selfcar icon" />
+    <h7>Selfcare</h7>
     <p></p>
   </div>
 )}
