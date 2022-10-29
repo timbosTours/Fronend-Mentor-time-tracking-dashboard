2b11f94f4b0528226c116adfe37641046a5b7a1b
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-10-29 14:29:36 +1100

    rendered json title dynamically

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,96 +1,44 @@
 import './App.css';
 import data from './data.json';
-// import avatar from './images/image-jeremy.png';
-import work from './images/icon-work.svg';
-// import play from './images/icon-play.svg';
-// import study from './images/icon-study.svg';
-// import exercise from './images/icon-exercise.svg';
-// import social from './images/icon-social.svg';
-// import selfcare from './images/icon-self-care.svg';
-
+import avatar from './images/image-jeremy.png';
 
 function App() {
-  <Work/>
-  // return (
-  //   <div className="user content-box">
-  //     <img src={ avatar } alt="user image" />
-  //     <h1>Report for</h1>
-  //     <p>Jeremy Robson</p>
-  //   </div>
-
-
+  return (<main>
+    <User/>
+    <div >
+      {data.map(activity => {
+        return (
+          <Activity key={activity.title} activity={activity}/>
+        );
+      })}
+    </div>
     
-      // data.map(timeData => {
-      // return (
-      //   <div className="user content-box">
-      //     <img src={ work } alt="" />
-      //     <h1>{timeData.title}</h1>
-      //     <p>{ timeData.timeframes.daily }</p>
-      //   </div>
-    //   )
-    // })
+
+  </main>
+  )
 }
 
-// const User = () => {
-//   return (
-//     <div className="user content-box">
-//       <img src={ avatar } alt="user image" />
-//       <h1>Report for</h1>
-//       <p>Jeremy Robson</p>
-//     </div>);
-// }
-const Work = () => {
+const User = () => {
   return (
-    data.map(timeData => {
-      return (
-        <div className="user content-box">
-          <img src={work} alt="" />
-          <h1>{timeData.title}</h1>
-          <p>{timeData.timeframes.daily}</p>
-        </div>
-      )
-    }))
+    <div className="user">
+      <img src={avatar} alt="" />
+      <h1>Report for</h1>
+      <h2>Jeremy Robson</h2>
+    </div>
+  )
 }
 
-// const Play = () => {
-//   return (
-//   <div className="play content-box">
-//     <img src={ play } alt="play icon" />
-//     <h1>Play</h1>
-//     <p></p>
-//   </div>
-// )}
-// const Study = () => {
-//   return (
-//   <div className="study content-box">
-//     <img src={ study } alt="study icon" />
-//     <h1>Study</h1>
-//     <p></p>
-//   </div>
-// )}
-// const Exercise = () => {
-//   return (
-//   <div className="excersice content-box">
-//     <img src={ exercise } alt="exercise icon" />
-//     <h1>Exercise</h1>
-//     <p></p>
-//   </div>
-// )}
-// const Social = () => { 
-//   return(
-//   <div className="social content-box">
-//     <img src={ social } alt="social icon" />
-//     <h1>Social</h1>
-//     <p></p>
-//   </div>
-// )}
-// const Selfcare = () => {
-//   return (
-//   <div className="selfcare content-box">
-//     <img src={ selfcare } alt="selfcar icon" />
-//     <h1>Selfcare</h1>
-//     <p></p>
-//   </div>
-// )}
+const Activity = (props) => {
+  const { title } = props.activity;
+  return (
+    <div className='content-box'>
+
+      <h1>{title}</h1>
+      
+    </div>
+  );
+
+}
+
 
 export default App;
