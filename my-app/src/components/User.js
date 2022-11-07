d6de87faccb4d8e45097c699dc0c2b2b14c72142
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-11-08 07:02:37 +1100

    radio btns

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -6,9 +6,15 @@ const User = () => {
     <div className="user">
         <img src={avatar} alt="" />
         <h1>Report for</h1>
-        <h2>Jeremy Robson</h2>
+            <h2>Jeremy Robson</h2>
+            
+            <form>
+                <button onClick={console.log('daily')} className="radio-btn" type="radio">Daily</button>
+                <button onClick={console.log('weekly')} className="radio-btn" type="radio">Weekly</button>
+                <button onClick={console.log('monthly')} className="radio-btn" type="radio">Monthly</button>
+            </form>
     </div>
-  )
+)
 }
 
 export default User
