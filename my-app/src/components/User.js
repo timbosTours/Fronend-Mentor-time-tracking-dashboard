92d6c8a867a764a64bb19ea8e4362006fedfa360
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-11-13 12:06:46 +1100

    conditionally rendered hours

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -1,60 +1,25 @@
-import React, { useState } from 'react'; 
+import React, {useState} from 'react'; 
 import avatar from '../images/image-jeremy.png';
 import data from '../data.json';
 
 
 
+
 const User = () => {
-    const [timeFrame, setTime] = useState()
 
-    function setTimeframe(value) {
-        setTime(()=>{
-    if (value === 1) {
-        console.log(`you clicked ${value}`)
-        data.map((_data) => {
-            return (
-                <div>
-                    <h1>{_data.timeframes.daily.current}</h1>
-                    <h1>{_data.timeframes.daily.previous}</h1>
-                </div>)
-        })
-    } else
-        if (value === 2) {
-            console.log(`you clicked ${value}`)
-            data.map((_data) => {
-                return (
-                    <div>
-                        <h1>{_data.timeframes.weekly.current}</h1>
-                        <h1>{_data.timeframes.weekly.previous}</h1>
-                    </div>
-                )
-            })
-        } else
-            if (value === 3) {
-                console.log(`you clicked ${value}`)
-                data.map((_data) => {
-                    return (
-                        <div>
-                            <h1>{_data.timeframes.monthly.current}</h1>
-                            <h1>{_data.timeframes.monthly.previous}</h1>
-                        </div>
-                    )
-                })
-                return timeFrame
-            }})
-}
+    const [value, setValue] = useState(2)
     
-
     return (
         <div className="user">
             <img src={avatar} alt="" />
             <h1>Report for</h1>
             <h2>Jeremy Robson</h2>
             
+            
             <form className='menu'>
-                <button key={'daily'} onClick={() => { setTimeframe(1) }} className='daily' >Daily</button>
-                <button key={'weekly'} onClick={() => { setTimeframe(2) }} className='weekly' >Weekly</button>
-                <button key={'monthly'} onClick={() => { setTimeframe(3) }} className='monthly'
+                <button key={'daily'} onClick={() => { setValue(1) }} className='daily' >Daily</button>
+                <button key={'weekly'} onClick={() => { setValue(2) }} className='weekly' >Weekly</button>
+                <button key={'monthly'} onClick={() => { setValue(3) }} className='monthly'
                 >Monthly</button>
             </form>
             {data.map(activityData => (
@@ -63,7 +28,10 @@ const User = () => {
                     <img className={`icon-${activityData.title.toLowerCase()}`} src={activityData.img} alt="" />
                     <h1>{activityData.title}</h1>
                     <ul>
-                        <li>{timeFrame}</li>
+                        {value === 1 ?
+                            <h1>{activityData.timeframes.daily.current}hrs</h1> : null}
+                        {value === 2 ? <h1>{activityData.timeframes.weekly.current}hrs</h1> : null}
+                        {value === 3 ? <h1>{activityData.timeframes.monthly.current}hrs</h1> : null}
                 </ul>
                 </div>
             ))}
