beb36da5285941c469d70ea82826b08ee27e807d
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-11-13 14:44:04 +1100

    refactored using props

---
+++ my-app/src/components/Activity.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import data from '../data.json'
+
+function Activity(props) {
+    return (
+    data.map(activityData => (
+                <div key={activityData.title} className='content-box'>
+                
+                    <img className={`icon-${activityData.title.toLowerCase()}`} src={activityData.img} alt="" />
+                    <h1>{activityData.title}</h1>
+                    <ul>
+                        {props.value === 1 ?
+                            <div>
+                                <h1>{activityData.timeframes.daily.current}hrs</h1>
+                                <h2>Yesterday - {activityData.timeframes.daily.previous}</h2>
+
+                            </div>
+                            : null}
+                        {props.value === 2 ?
+                            <div>
+                                <h1>{activityData.timeframes.weekly.current}hrs</h1>
+                                <h2>Last Week - {activityData.timeframes.daily.previous}</h2>
+                            </div>
+                            : null}
+                        {props.value === 3 ?
+                            <div>
+                                <h1>{activityData.timeframes.monthly.current}hrs</h1>
+                                <h2>Last Month - {activityData.timeframes.daily.previous}</h2>
+                            </div>
+                            : null}
+                </ul>
+                </div>
+            )))}
+
+
+export default Activity
--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react'; 
 import avatar from '../images/image-jeremy.png';
-import data from '../data.json';
+import Activity from './Activity';
 
 
 
 
 const User = () => {
-
+    // se
     const [value, setValue] = useState(2)
     
     return (
@@ -22,19 +22,7 @@ const User = () => {
                 <button key={'monthly'} onClick={() => { setValue(3) }} className='monthly'
                 >Monthly</button>
             </form>
-            {data.map(activityData => (
-                <div key={activityData.title} className='content-box'>
-                
-                    <img className={`icon-${activityData.title.toLowerCase()}`} src={activityData.img} alt="" />
-                    <h1>{activityData.title}</h1>
-                    <ul>
-                        {value === 1 ?
-                            <h1>{activityData.timeframes.daily.current}hrs</h1> : null}
-                        {value === 2 ? <h1>{activityData.timeframes.weekly.current}hrs</h1> : null}
-                        {value === 3 ? <h1>{activityData.timeframes.monthly.current}hrs</h1> : null}
-                </ul>
-                </div>
-            ))}
+            <Activity value={value} setValue={setValue} />
         </div>
     )
 }
