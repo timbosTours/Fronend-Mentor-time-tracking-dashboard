7a0ecf9458fa9a0b87e56cf97c2fc078aa67d548
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-11-18 06:27:45 +1100

    activity fonts/weights/size/color

--- my-app/src/components/Activity.js
+++ my-app/src/components/Activity.js
@@ -8,28 +8,28 @@ function Activity(props) {
         // map data to different elements
         data.map(activityData => (
                 <div key={activityData.title} className='content-box'>
-                
+                    
                     <img className={`icon-${activityData.title.toLowerCase()}`} src={activityData.img} alt="" />
-                    <h1>{activityData.title}</h1>
+                    <h1 className='activity-title'>{activityData.title}</h1>
                 <ul>
                     {/* use conditional rendering to render correct data timeframe based on button input(passed down as props from user component) */}
                         {props.value === 1 ?
                             <div>
-                                <h1>{activityData.timeframes.daily.current}hrs</h1>
-                                <h2>Yesterday - {activityData.timeframes.daily.previous}</h2>
+                                <h1 className='hours'>{activityData.timeframes.daily.current}hrs</h1>
+                                <h2 className='previous'>Yesterday - {activityData.timeframes.daily.previous}</h2>
 
                             </div>
                             : null}
                         {props.value === 2 ?
                             <div>
-                                <h1>{activityData.timeframes.weekly.current}hrs</h1>
-                                <h2>Last Week - {activityData.timeframes.daily.previous}</h2>
+                                <h1 className='hours'>{activityData.timeframes.weekly.current}hrs</h1>
+                                <h2 className='previous'>Last Week - {activityData.timeframes.daily.previous}</h2>
                             </div>
                             : null}
                         {props.value === 3 ?
                             <div>
-                                <h1>{activityData.timeframes.monthly.current}hrs</h1>
-                                <h2>Last Month - {activityData.timeframes.daily.previous}</h2>
+                                <h1 className='hours'>{activityData.timeframes.monthly.current}hrs</h1>
+                                <h2 className='previous'>Last Month - {activityData.timeframes.daily.previous}</h2>
                             </div>
                             : null}
                 </ul>
